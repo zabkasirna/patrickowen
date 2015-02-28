b376bd66101d85a8d756d7a1ccef8391326db462
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-01 05:56:11 +0700

    Clean up. Next, write other plugins.

--- src/template/index.php
+++ src/template/index.php
@@ -7,25 +7,27 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-            <p><?php the_title(); ?></p>
-            <section>
-                <?php the_content(); ?>
-            </section>
-
-            <?php endwhile; ?>
-
-        <?php else : ?>
-
-            <p>no post</p>
-
-        <?php endif; ?>
-
-        </main><!-- .site-main -->
-    </div><!-- .content-area -->
+    <div id="content">
+        <div id="inner-content">
+            <main id="main" role="main">
+        
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        
+                <p><?php the_title(); ?></p>
+                <section>
+                    <?php the_content(); ?>
+                </section>
+        
+                <?php endwhile; ?>
+        
+            <?php else : ?>
+        
+                <p>no home</p>
+        
+            <?php endif; ?>
+        
+            </main><!-- #main -->
+        </div><!-- #inner-content -->
+    </div>
 
 <?php get_footer(); ?>
