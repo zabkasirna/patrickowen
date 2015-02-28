9ca2f8f977b13eb68d5b3350b02a5366516429ac
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-01 05:52:11 +0700

    Finish major rewrite for campaign cpt plugin.

--- src/plugin/po-campaign/po-campaign.php
+++ src/plugin/po-campaign/po-campaign.php
@@ -12,104 +12,98 @@ Author: Sirna
 Version: 0.0.0
 Author URI: http://github.com/zabkasirna
 */
+
+// Register Custom Post Type
 function register_cpt_campaigns() {
- 
-    $labels = array( 
-        'name' => _x( 'campaign', 'campaigns' ),
-        'singular_name' => _x( 'campaigns', 'campaigns' ),
-        'add_new' => _x( 'Add New', 'campaigns' ),
-        'add_new_item' => _x( 'Add New Campaigns', 'campaigns' ),
-        'edit_item' => _x( 'Edit Campaigns', 'campaigns' ),
-        'new_item' => _x( 'New Campaigns', 'campaigns' ),
-        'view_item' => _x( 'View Campaigns', 'campaigns' ),
-        'search_items' => _x( 'Search Campaign', 'campaigns' ),
-        'not_found' => _x( 'No campaign found', 'campaigns' ),
-        'not_found_in_trash' => _x( 'No campaign found in Trash', 'campaigns' ),
-        'parent_item_colon' => _x( 'Parent Campaigns:', 'campaigns' ),
-        'menu_name' => _x( 'Campaign', 'Campaigns' ),
+
+    $labels = array(
+        'name'                => _x( 'Campaigns', 'Post Type General Name', 'text_domain' ),
+        'singular_name'       => _x( 'Campaign', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'           => __( 'Campaign', 'text_domain' ),
+        'parent_item_colon'   => __( 'Parent Campaign:', 'text_domain' ),
+        'all_items'           => __( 'All Campaigns', 'text_domain' ),
+        'view_item'           => __( 'View Campaign', 'text_domain' ),
+        'add_new_item'        => __( 'Add New Campaign', 'text_domain' ),
+        'add_new'             => __( 'Add New Campaigns', 'text_domain' ),
+        'edit_item'           => __( 'Edit Campaign', 'text_domain' ),
+        'update_item'         => __( 'Update Campaign', 'text_domain' ),
+        'search_items'        => __( 'Search Campaign', 'text_domain' ),
+        'not_found'           => __( 'Campaign Not found', 'text_domain' ),
+        'not_found_in_trash'  => __( 'Campaign Not found in Trash', 'text_domain' ),
     );
- 
-    $args = array( 
-        'labels' => $labels,
-        'hierarchical' => false,
-        'description' => 'campaign custom post',
-        'supports' => array( 'title', 'editor', 'thumbnail' ),
-        'taxonomies' => array( 'campaign_season' ),
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'menu_position' => 5,
-        
-        'show_in_nav_menus' => true,
-        'publicly_queryable' => true,
+    $rewrite = array(
+        'slug'                => 'campaign',
+        'with_front'          => true,
+        'pages'               => false,
+        'feeds'               => true,
+    );
+    $args = array(
+        'label'               => __( 'campaign', 'text_domain' ),
+        'description'         => __( 'Campaign CPT', 'text_domain' ),
+        'labels'              => $labels,
+        'supports'            => array( 'title', 'editor', 'thumbnail', ),
+        'taxonomies'          => array( 'Seasons' ),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-images-alt2',
+        'can_export'          => true,
+        'has_archive'         => true,
         'exclude_from_search' => false,
-        'has_archive' => true,
-        'query_var' => true,
-        'can_export' => true,
-        'rewrite' => true,
-        'capability_type' => 'post'
+        'publicly_queryable'  => true,
+        'query_var'           => 'campaign',
+        'rewrite'             => $rewrite,
+        'capability_type'     => 'post',
     );
- 
     register_post_type( 'campaign', $args );
-}
- 
-add_action( 'init', 'register_cpt_campaigns' );
- 
-function register_taxonomy_campaign_season() {
 
-    $labels = array( 
-        'name' => _x( 'campaign-seasons', 'seasons' ),
-        'singular_name' => _x( 'campaign-season', 'seasons' ),
-        'search_items' => _x( 'Search seasons', 'seasons' ),
-        'popular_items' => _x( 'Popular seasons', 'seasons' ),
-        'all_items' => _x( 'All seasons', 'seasons' ),
-        'parent_item' => _x( 'Parent campaign-season', 'seasons' ),
-        'parent_item_colon' => _x( 'Parent campaign-season:', 'seasons' ),
-        'edit_item' => _x( 'Edit season', 'seasons' ),
-        'update_item' => _x( 'Update season', 'seasons' ),
-        'add_new_item' => _x( 'Add New season', 'seasons' ),
-        'new_item_name' => _x( 'New season', 'seasons' ),
-        'separate_items_with_commas' => _x( 'Separate campaign-seasons with commas', 'seasons' ),
-        'add_or_remove_items' => _x( 'Add or remove seasons', 'seasons' ),
-        'choose_from_most_used' => _x( 'Choose from most used seasons', 'seasons' ),
-        'menu_name' => _x( 'Seasons', 'Seasons' ),
-    );
-
-    $args = array( 
-        'labels' => $labels,
-        'public' => true,
-        'show_in_nav_menus' => false,
-        'show_ui' => true,
-        'show_tagcloud' => true,
-        'show_admin_column' => true,
-        'hierarchical' => false,
-        'update_count_callback' => 'season_count_update_cb',
-        'rewrite' => true,
-        'query_var' => true
-    );
-
-    register_taxonomy( 'campaign_season', array('campaign'), $args );
 }
 
-add_action( 'init', 'register_taxonomy_campaign_season' );
+// Hook into the 'init' action
+add_action( 'init', 'register_cpt_campaigns', 0 );
 
-function create_campaign_pages()
-  {
-   //post status and options
-    $post = array(
-          'post_name' => 'campaign',
-          'post_type' => 'post',
-          'post_title' => wp_strip_all_tags( $_POST['post_title'] ),
-          'post_date' => date('Y-m-d H:i:s'),
-          'ping_status' =>  'closed' ,
-          'comment_status' => 'closed'
-    );
-    //insert page and save the id
-    $newvalue = wp_insert_post( $post, false );
-    //save the id in the database
-    update_option( 'campaignpage', $newvalue );
-  }
+/**
+ * Include the template files from the plugin dir
+ * [http://code.tutsplus.com/tutorials/a-guide-to-wordpress-custom-post-types-taxonomies-admin-columns-filters-and-archives--wp-27898]
+ */
 
-register_activation_hook( __FILE__, 'create_campaign_pages');
+function include_campaign_template() {
+    
+    if ( get_post_type() == 'campaign' ) {
+
+
+        // Include single template
+        if ( is_single() ) {
+
+            if ( $theme_file = locate_template( array( 'single-campaign.php' ) ) ) {
+                $template_path = $theme_file;
+            }
+
+            else {
+                $template_path = plugin_dir_path( __FILE__ ) . 'template/single-campaign.php';
+            }
+        }
+
+        // Include archive template
+        else if ( is_archive() ) {
+
+            if ( $theme_file = locate_template( array( 'archive-campaign.php' ) ) ) {
+                $template_path = $theme_file;
+            }
+
+            else {
+                $template_path = plugin_dir_path( __FILE__ ) . 'template/archive-campaign.php';
+            }
+        }
+    }
+
+    return $template_path;
+}
+
+add_filter( 'template_include', 'include_campaign_template', 1 );
 
 ?>
