4b7c1fe6d56d6ed2e0a2a14f38501f7cec76fbae
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-04 01:29:43 +0700

    Start headline.

--- src/plugin/po-campaign/template/archive-campaign.php
+++ src/plugin/po-campaign/template/archive-campaign.php
@@ -13,10 +13,19 @@ get_header(); ?>
         
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         
-                <p><?php the_title(); ?></p>
-                <section>
-                    <?php the_content(); ?>
-                </section>
+                <div class="fullheight-row">
+                    <div class="headline">
+                        <div class="headline-tax-outer">
+                            <p class="headline-tax"><?php echo get_post_type(); ?></p>
+                        </div>
+                        <div class="title-outer">
+                            <h1 class="title"><?php the_title(); ?></h1>
+                        </div>
+                    </div>
+                    <section>
+                        <?php the_content(); ?>
+                    </section>
+                </div>
         
                 <?php endwhile; ?>
         
