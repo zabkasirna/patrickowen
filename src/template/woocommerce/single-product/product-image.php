74f758919ac6151fb7742ed9fabebd8d8897f7b2
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-03 19:08:13 +0700

    Modify product templates.

--- src/template/woocommerce/single-product/product-image.php
+++ src/template/woocommerce/single-product/product-image.php
@@ -37,7 +37,7 @@ global $post, $woocommerce, $product;
 
 		} else {
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
+			// echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
 
 		}
 	?>
