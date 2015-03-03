b45b707d1c7c44a4ff57f2c6715da597d439a6b3
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-04 01:30:32 +0700

    Install waypoints from bower. Modify script enqueue.

--- src/template/index.php
+++ src/template/index.php
@@ -7,11 +7,8 @@
 
 get_header(); ?>
 
-<nav>
-    <?php wp_nav_menu (array('theme_location' => 'main-navi','menu_class' => 'nav'));?>
-</nav>
-
 <div>
     <?php the_content();?>
 </div>
+
 <?php get_footer(); ?>
--- src/template/sirna-po15.php
+++ src/template/sirna-po15.php
@@ -56,11 +56,23 @@ function po_scripts_and_styles() {
 
     if (!is_admin()) {
 
+        // modernizr
+        wp_register_script( 'po-modernizr', get_template_directory_uri() . '/script/vendor/modernizr/modernizr.js', array(), '', false );
+
         // jquery
         wp_register_script( 'po-jquery', get_template_directory_uri() . '/script/vendor/jquery/dist/jquery.js', array(), '', true );
 
-        // modernizr
-        wp_register_script( 'po-modernizr', get_template_directory_uri() . '/script/vendor/modernizr/modernizr.js', array(), '', false );
+        // waypoints
+        wp_register_script( 'po-waypoints', get_template_directory_uri() . '/script/vendor/waypoints/lib/jquery.waypoints.js', array( 'po-jquery' ), '', true );
+
+        // site
+        // wp_register_script( 'po-js', get_stylesheet_directory_uri() . '/script/main.js', array( 'jquery' ), '', true );
+
+        // enqueue scripts
+        wp_enqueue_script( 'po-modernizr' );
+        wp_enqueue_script( 'po-jquery' );
+        wp_enqueue_script( 'po-waypoints' );
+        // wp_enqueue_script( 'po-js' );
 
         // main stylesheet
         wp_register_style( 'po-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), '', 'all' );
@@ -69,18 +81,11 @@ function po_scripts_and_styles() {
         // @todo: Develop ie-only stylesheet
         wp_register_style( 'po-stylesheet', get_stylesheet_directory_uri() . '/style.ie.css', array(), '' );
 
-        // adding scripts file in the footer
-        // wp_register_script( 'po-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
-
-        // enqueue styles and scripts
-        wp_enqueue_script( 'po-modernizr' );
+        // enqueue styles
         wp_enqueue_style( 'po-stylesheet' );
         wp_enqueue_style( 'po-ie-only' );
 
         $wp_styles->add_data( 'po-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
-
-        wp_enqueue_script( 'po-jquery' );
-        // wp_enqueue_script( 'po-js' );
     }
 }
 
