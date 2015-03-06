0a6fe6c1d6a25430de52c8b18c9a146586b40da7
Emirsyaf Munir <EM@Emirsyaf-MacBook-Pro.local>
2015-03-06 19:04:07 +0700

    Newsletter done.

---
+++ src/script/Newsletter.js
@@ -0,0 +1,14 @@
+var Newsletter = {
+    init: {
+        selecter: initSelect
+    }
+};
+
+function initSelect() {
+    if ( !$( '.wysija-select' ).length ) return;
+    var $el = $( '.wysija-select' );
+
+    $el.selecter();
+}
+
+module.exports = Newsletter;
