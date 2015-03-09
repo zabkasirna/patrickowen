c79c67555ce0ea4b2f94e34334080959f3572238
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-09 23:10:24 +0700

    prepare.

--- src/plugin/sirna-hotspot/acf-sirna_hotspot-v4.php
+++ src/plugin/sirna-hotspot/acf-sirna_hotspot-v4.php
@@ -5,15 +5,14 @@ class acf_field_sirna_hotspot extends acf_field {
     // vars
     var $settings, // will hold info such as dir / path
         $defaults; // will hold default field options
-        
-        
+
     /*
     *  __construct
     *
     *  Set name / label needed for actions / filters
     *
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
     
     function __construct()
@@ -21,7 +20,7 @@ class acf_field_sirna_hotspot extends acf_field {
         // vars
         $this->name = 'sirna_hotspot';
         $this->label = __('Sirna Hotspot');
-        $this->category = __("Basic",'acf'); // Basic, Content, Choice, etc
+        $this->category = __("Content",'acf'); // Basic, Content, Choice, etc
         $this->defaults = array(
             'preview_size' => 'full'
         );
@@ -44,12 +43,12 @@ class acf_field_sirna_hotspot extends acf_field {
     /*
     *  create_options()
     *
-    *  Create extra options for your field. This is rendered when editing a field.
+    *  Create extra options for field. Rendered when editing a field.
     *  The value of $field['name'] can be used (like below) to save extra data to the $field
     *
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $field  - an array holding all the field's data
     */
@@ -68,26 +67,7 @@ class acf_field_sirna_hotspot extends acf_field {
         // Create Field Options HTML
         ?>
 <tr class="field_option field_option_<?php echo $this->name; ?>">
-    <td class="label">
-        <label><?php _e("Preview Size",'acf'); ?></label>
-        <p class="description"><?php _e("Thumbnail is advised",'acf'); ?></p>
-    </td>
-    <td>
-        <?php
-        
-        do_action('acf/create_field', array(
-            'type'      =>  'radio',
-            'name'      =>  'fields['.$key.'][preview_size]',
-            'value'     =>  $field['preview_size'],
-            'layout'    =>  'horizontal',
-            'choices'   =>  array(
-                'thumbnail' => __('Thumbnail'),
-                'something_else' => __('Something Else'),
-            )
-        ));
-        
-        ?>
-    </td>
+
 </tr>
         <?php
         
@@ -97,13 +77,13 @@ class acf_field_sirna_hotspot extends acf_field {
     /*
     *  create_field()
     *
-    *  Create the HTML interface for your field
+    *  Create the field HTML interface
     *
     *  @param   $field - an array holding all the field's data
     *
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
     
     function create_field( $field )
@@ -128,25 +108,21 @@ class acf_field_sirna_hotspot extends acf_field {
     /*
     *  input_admin_enqueue_scripts()
     *
-    *  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
-    *  Use this action to add CSS + JavaScript to assist your create_field() action.
+    *  This action is called in the admin_enqueue_scripts action on the edit screen where the field is created.
+    *  Use this action to add CSS + JavaScript to assist create_field() action.
     *
     *  $info    http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
 
     function input_admin_enqueue_scripts()
     {
-        // Note: This function can be removed if not used
-        
-        
         // register ACF scripts
         wp_register_script( 'acf-input-sirna_hotspot', $this->settings['dir'] . 'js/input.js', array('acf-input'), $this->settings['version'] );
         wp_register_style( 'acf-input-sirna_hotspot', $this->settings['dir'] . 'css/input.css', array('acf-input'), $this->settings['version'] ); 
-        
-        
+
         // scripts
         wp_enqueue_script(array(
             'acf-input-sirna_hotspot',  
@@ -156,21 +132,19 @@ class acf_field_sirna_hotspot extends acf_field {
         wp_enqueue_style(array(
             'acf-input-sirna_hotspot',  
         ));
-        
-        
     }
     
     
     /*
     *  input_admin_head()
     *
-    *  This action is called in the admin_head action on the edit screen where your field is created.
-    *  Use this action to add CSS and JavaScript to assist your create_field() action.
+    *  This action is called in the admin_head action on the edit screen where the field is created.
+    *  Use this action to add CSS and JavaScript to assist the create_field() action.
     *
     *  @info    http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
 
     function input_admin_head()
@@ -182,13 +156,13 @@ class acf_field_sirna_hotspot extends acf_field {
     /*
     *  field_group_admin_enqueue_scripts()
     *
-    *  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
-    *  Use this action to add CSS + JavaScript to assist your create_field_options() action.
+    *  This action is called in the admin_enqueue_scripts action on the edit screen where the field is edited.
+    *  Use this action to add CSS + JavaScript to assist the create_field_options() action.
     *
     *  $info    http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
 
     function field_group_admin_enqueue_scripts()
@@ -200,13 +174,13 @@ class acf_field_sirna_hotspot extends acf_field {
     /*
     *  field_group_admin_head()
     *
-    *  This action is called in the admin_head action on the edit screen where your field is edited.
-    *  Use this action to add CSS and JavaScript to assist your create_field_options() action.
+    *  This action is called in the admin_head action on the edit screen where the field is edited.
+    *  Use this action to add CSS and JavaScript to assist the create_field_options() action.
     *
     *  @info    http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
     *  @type    action
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     */
 
     function field_group_admin_head()
@@ -221,8 +195,8 @@ class acf_field_sirna_hotspot extends acf_field {
         *  This filter is applied to the $value after it is loaded from the db
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $value - the value found in the database
     *  @param   $post_id - the $post_id from which the value was loaded
@@ -233,7 +207,6 @@ class acf_field_sirna_hotspot extends acf_field {
     
     function load_value( $value, $post_id, $field )
     {
-        // Note: This function can be removed if not used
         return $value;
     }
     
@@ -244,8 +217,8 @@ class acf_field_sirna_hotspot extends acf_field {
     *  This filter is applied to the $value before it is updated in the db
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $value - the value which will be saved in the database
     *  @param   $post_id - the $post_id of which the value will be saved
@@ -256,7 +229,6 @@ class acf_field_sirna_hotspot extends acf_field {
     
     function update_value( $value, $post_id, $field )
     {
-        // Note: This function can be removed if not used
         return $value;
     }
     
@@ -267,8 +239,8 @@ class acf_field_sirna_hotspot extends acf_field {
     *  This filter is applied to the $value after it is loaded from the db and before it is passed to the create_field action
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $value  - the value which was loaded from the database
     *  @param   $post_id - the $post_id from which the value was loaded
@@ -286,8 +258,6 @@ class acf_field_sirna_hotspot extends acf_field {
         
         // perhaps use $field['preview_size'] to alter the $value?
         
-        
-        // Note: This function can be removed if not used
         return $value;
     }
     
@@ -298,8 +268,8 @@ class acf_field_sirna_hotspot extends acf_field {
     *  This filter is applied to the $value after it is loaded from the db and before it is passed back to the API functions such as the_field
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $value  - the value which was loaded from the database
     *  @param   $post_id - the $post_id from which the value was loaded
@@ -316,9 +286,7 @@ class acf_field_sirna_hotspot extends acf_field {
         */
         
         // perhaps use $field['preview_size'] to alter the $value?
-        
-        
-        // Note: This function can be removed if not used
+
         return $value;
     }
     
@@ -329,8 +297,8 @@ class acf_field_sirna_hotspot extends acf_field {
     *  This filter is applied to the $field after it is loaded from the database
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $field - the field array holding all the field options
     *
@@ -339,7 +307,6 @@ class acf_field_sirna_hotspot extends acf_field {
     
     function load_field( $field )
     {
-        // Note: This function can be removed if not used
         return $field;
     }
     
@@ -350,8 +317,8 @@ class acf_field_sirna_hotspot extends acf_field {
     *  This filter is applied to the $field before it is saved to the database
     *
     *  @type    filter
-    *  @since   3.6
-    *  @date    23/01/13
+    *  @since   0.0.1
+    *  @date    09/03/15
     *
     *  @param   $field - the field array holding all the field options
     *  @param   $post_id - the field group ID (post_type = acf)
@@ -361,7 +328,6 @@ class acf_field_sirna_hotspot extends acf_field {
 
     function update_field( $field, $post_id )
     {
-        // Note: This function can be removed if not used
         return $field;
     }
 
