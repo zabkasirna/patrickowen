9dcd94bb324edb5c2d1011fd8beb20adf23f2112
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-03-09 17:06:46 +0700

    Cleanup.

--- src/template/sirna-po15.php
+++ src/template/sirna-po15.php
@@ -246,12 +246,12 @@ function powc_remove_content() {
 
 function powc_cleanup() {
     add_filter( 'woocommerce_enqueue_styles', 'powc_dequeue_styles' );
+    add_filter( 'woocommerce_product_tabs', 'powc_remove_review_tab' );
     
     remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
     remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10, 0 );
 
     add_action( 'woocommerce_before_shop_loop_item_title', 'powc_template_loop_product_thumbnail', 10, 0 );
-
     add_action( 'init', 'powc_remove_content' );
 }
 
